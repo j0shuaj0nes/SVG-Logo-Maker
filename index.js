16c3b97288954267ff7356d8eb743fd25df4ced8
Josh Jones <9joshuajones5@gmail.com>
2023-12-19 22:31:27 +0800

    added js

---
+++ index.js
@@ -0,0 +1,66 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+inquirer.prompt([
+    {
+        message: 'Choose three character for your logo',
+        name: 'logo',
+
+    },
+    {
+        message: 'Choose a colour for your logo',
+        name: 'logoColour',
+
+    },
+    {
+        message: 'Choose a shape colour for your logo',
+        name: 'shapeColour',
+
+    },
+    {
+        type: 'list',
+        message: 'Choose a shape for your logo',
+        name: 'logoShape',
+        choices: ['triangle', 'circle', 'square'],
+
+    },
+
+]).then(answers => {
+    if (answers.logoShape == 'triangle') {
+        fs.writeFileSync('logo.svg', `
+        <svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+		    <polygon points="150, 18 244, 182 56, 182" fill='${answers.shapeColour}'/>
+            <text x="150" y="100" fill="${answers.logoColour}">${answers.logo}</text>
+
+	  </svg>
+        `)
+    } 
+    
+    if (answers.logoShape == 'square') {
+        fs.writeFileSync('logo.svg', `
+        <svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+		    <rect width="200px" height="200px" fill="${answers.shapeColour}" />
+            <text x="100" y="100" fill="${answers.logoColour}">${answers.logo}</text>
+
+	  </svg>
+        `)
+    } 
+    
+    if (answers.logoShape == 'circle') {
+        fs.writeFileSync('logo.svg', `
+        <svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+		    <circle cx="150" cy="100" r="80" fill="${answers.shapeColour}" />
+            <text x="150" y="100" fill="${answers.logoColour}">${answers.logo}</text>
+
+	  </svg>
+        `)
+    } console.log('Generated logo.svg')
+});
+
+
---
+++ tests/shapes.test.js
@@ -0,0 +1,9 @@
+//const Triangle = require('../lib/shapes.js');
+
+//describe('Triangle', () => {
+  //test('should throw error if render() is called', () => {
+    //const Triangle = new Triangle();
+    //const err = new Error('Child class must implement a render() method.')
+    //expect(Triangle.render).toThrow(err);
+  //});
+//});
